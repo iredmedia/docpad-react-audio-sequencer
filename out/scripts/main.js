04b1768b28ce9e170110f1c534fab6d4ae4ae156
Kevin Redman <kevin.r@myplanetdigital.com>
2014-01-11 20:33:16 -0500

    Initial commit - React page working with grunt

---
+++ out/scripts/main.js
@@ -0,0 +1,4 @@
+/** @jsx React.DOM */
+React.renderComponent( React.DOM.h1(null ,  " Hello, world! " ),
+    document.getElementById('example')
+);
---
+++ out/scripts/main.jsx
@@ -0,0 +1,4 @@
+/** @jsx React.DOM */
+React.renderComponent( < h1 > Hello, world! < /h1>,
+    document.getElementById('example')
+);
